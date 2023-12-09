83c2788ac1efc400fac7d48d7d9c805df44363eb
FrancheskoDown <frollite2@gmail.com>
2023-12-09 12:27:59 +0300

    Made selected cell indicator
    -Added Selected Cell Indicator Mover

---
+++ Project Mononoke/Assets/Scripts/Source/SelectedCellIndicatorMover.cs
@@ -0,0 +1,18 @@
+using Base.Utils;
+using UnityEngine;
+
+namespace Source
+{
+    public class SelectedCellIndicatorMover : MonoBehaviour
+    {
+        [SerializeField] private Grid _grid;
+        private GameObject _indicator = null;
+
+        public void Update()
+        {
+            _indicator ??= gameObject;
+            var gridPosition = _grid.WorldToCell(MouseUtils.GetMouseWorldPosWithoutZUsingNewInputSystem());
+            _indicator.transform.position = _grid.CellToWorld(gridPosition);
+        }
+    }
+}
