1b6ed6522df5c0736eaaab70922988ff67d9cb5c
FrancheskoDown <frollite2@gmail.com>
2024-02-25 21:36:57 +0300

    Some code refactoring

--- Project Mononoke/Assets/Scripts/Source/Pick Up Module/PickUpper.cs
+++ Project Mononoke/Assets/Scripts/Source/Pick Up Module/PickUpper.cs
@@ -7,18 +7,20 @@ namespace Source.PickUpModule
    [RequireComponent(typeof(Collider2D))]
    public class PickUpper : MonoBehaviour
    {
-      public Inventory Inventory {get; private set;} = new Inventory(100, 100);
+      public Inventory Inventory {get; private set;} = new(weightCapacity: 100, volumeCapacity: 100);
+      public Hand Hand { get; private set; } = new(strength: 5, volume: 2);
 
 
       private void OnCollisionEnter2D(Collision2D other)
       {
          Inventory ??= new Inventory(100, 100);
+         Hand ??= new Hand(5, 2);
          
-         if(!other.gameObject.TryGetComponent<ItemView>(out var droppedItem)) return;
+         if(!other.gameObject.TryGetComponent<ItemView>(out var droppedItemView)) return;
 
-         if(!Inventory.TryAddItem(droppedItem.Item)) return;
+         if(!Hand.TryTake(droppedItemView.Item)) return;
 
-         droppedItem.BeginPickUp();
+         droppedItemView.BeginPickUp();
       }
    }
 }
