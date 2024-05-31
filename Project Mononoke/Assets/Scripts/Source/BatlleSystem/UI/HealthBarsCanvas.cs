8119ae9e7c92cc4f2d389229be5ce0f51dd6ec28
FrancheskoDown <frollite2@gmail.com>
2024-05-31 15:53:56 +0300

    Added updationg of position for healthbar

--- Project Mononoke/Assets/Scripts/Source/BatlleSystem/UI/HealthBarsCanvas.cs
+++ Project Mononoke/Assets/Scripts/Source/BatlleSystem/UI/HealthBarsCanvas.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Source.Character.Movement;
 using UnityEngine;
 
 namespace Source.BattleSystem.UI
@@ -5,18 +7,57 @@ namespace Source.BattleSystem.UI
     public class HealthBarsCanvas : MonoBehaviour
     {
         [SerializeField] private GameObject _healthBarPrefab = null;
+        private Dictionary<IsoCharacterMover, RectTransform> _spawnedHealthBarsForMobileObjects = null;
 
         public void AddHealthBarTo(StatsHolder statsHolder)
         {
-            Vector3 screenPosition = RectTransformUtility.WorldToScreenPoint(Camera.main, statsHolder.transform.position);
-            var healthBarObject = Instantiate(_healthBarPrefab, Vector3.zero, Quaternion.identity, transform);
-            var healthBarObjectRectTransform = healthBarObject.GetComponent<RectTransform>();
-            healthBarObjectRectTransform.SetParent(transform, false);
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(transform as RectTransform, screenPosition, Camera.main, out var localPoint);
-            healthBarObjectRectTransform.localPosition = localPoint;
+            SpawnHealthBarObject(out var healthBarObjectRectTransform);
+
+            UpdateHealthBarInCanvasPosition(statsHolder.transform.position, healthBarObjectRectTransform);
+
+            if (statsHolder.TryGetComponent<IsoCharacterMover>(out var mover))
+            {
+                _spawnedHealthBarsForMobileObjects ??= new Dictionary<IsoCharacterMover, RectTransform>();
+                _spawnedHealthBarsForMobileObjects.Add(mover, healthBarObjectRectTransform);
+                mover.MovementChanged += MovementChanged;
+            }
+
             healthBarObjectRectTransform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-            var healthBar = healthBarObject.GetComponentInChildren<HealthBar>();
+            
+            InitializeHealthBarWith(statsHolder, healthBarObjectRectTransform.gameObject.GetComponentInChildren<HealthBar>());
+        }
+
+        private void MovementChanged(object sender, IsoCharacterMover.MovementActionEventArgs e)
+        {
+            var mover = (IsoCharacterMover)sender;
+            UpdateHealthBarInCanvasPosition(mover.transform.position, _spawnedHealthBarsForMobileObjects[mover]);
+        }
+
+        private void UpdateHealthBarInCanvasPosition(Vector3 worldPosition, RectTransform healthBarObjectRectTransform)
+        {
+            var inCanvasHealthBarPosition = CalculateHealthBarsLocalCanvasPosition(worldPosition);
+            healthBarObjectRectTransform.position = inCanvasHealthBarPosition;
+        }
+
+        private GameObject SpawnHealthBarObject(out RectTransform healthBarObjectRectTransform)
+        {
+            var healthBarObject = Instantiate(_healthBarPrefab, Vector3.zero, Quaternion.identity, transform);
+            healthBarObjectRectTransform = healthBarObject.GetComponent<RectTransform>();
+            healthBarObjectRectTransform.SetParent(transform, false);
+            return healthBarObject;
+        }
+
+        private static void InitializeHealthBarWith(StatsHolder statsHolder, HealthBar healthBar)
+        {
             healthBar.Initialize(statsHolder);
+        }
+
+        private Vector3 CalculateHealthBarsLocalCanvasPosition(Vector3 worldPosition)
+        {
+            var screenPosition = RectTransformUtility.WorldToScreenPoint(Camera.main, worldPosition);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(transform as RectTransform, screenPosition, Camera.main,
+                out var localPoint);
+            return localPoint;
         }
     }
 }
