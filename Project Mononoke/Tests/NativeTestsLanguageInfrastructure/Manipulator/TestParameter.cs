ad33a416cee7e4583b117d0bba0effcc402298fe
FrancheskoDown <51823882+FrancheskoDown@users.noreply.github.com>
2024-07-13 18:25:44 +0300

    Added Cinemachine

---
+++ Project Mononoke/Tests/NativeTestsLanguageInfrastructure/Manipulator/TestParameter.cs
@@ -0,0 +1,8 @@
+namespace Tests.NativeTestsLanguageInfrastructure.Manipulator
+{
+    public class TestParameter
+    {
+        public float Strength = 0f;
+        public float Capacity = 0f;
+    }
+}
